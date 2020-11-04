51b2950278dc25f6d449fdbd45de2a8897b33093
Suny Gao <gaos@newschool.edu>
2020-11-04 13:22:34 -0500

    grid updates

--- whitley-darby-toy-proj-6/js/global.js
+++ whitley-darby-toy-proj-6/js/global.js
@@ -1,5 +1,17 @@
 window.addEventListener("load",function() {
-const grid = document.getElementsByClassName(".grid-container")
+    const grid = document.getElementsByClassName(".grid-container");
+
+    var squares = document.querySelectorAll('.square');
+
+    function changeColor(e) {
+        var square = e.target;
+        square.dataset.hovered = true;
+    }
+
+    squares.forEach(function(square) {
+        square.addEventListener('mouseover', changeColor);
+    });
+    
 	
 
 
