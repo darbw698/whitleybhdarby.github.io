eff1baff12a9713242680dd9e0f37407e7d9ac81
Whitley Darby <64851791+whitleybhdarby@users.noreply.github.com>
2020-11-03 20:46:51 -0500

    javascript has a example in it

--- whitley-darby-toy-proj-6/js/global.js
+++ whitley-darby-toy-proj-6/js/global.js
@@ -1,3 +1,16 @@
 window.addEventListener("load",function() {
-
+	// draw();
+	// function draw() {
+	// 	$('.dot').on('mouseenter', function() {
+	// 		$(this).addClass('draw');
+	// 	});
+	// }
+	
+	// function vanish() {
+	// 	$('.dot').on('mouseenter', function() {
+	// 		$(this).fadeTo('slow', 0);
+	// 	});
+	// }
+	
+	
 })
